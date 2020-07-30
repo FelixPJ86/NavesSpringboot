2a111b7c35f99f5fe7c0b9b6ec41058d53fa35fa
Félix <felix.potente@alten.es>
2020-07-30 17:32:08 +0200

    Rest SpringBoot

--- src/main/java/org/felix/ProyectoNavesApplication.java
+++ src/main/java/org/felix/ProyectoNavesApplication.java
@@ -1,6 +1,5 @@
 package org.felix;
 
-import org.apache.struts2.dispatcher.filter.StrutsPrepareAndExecuteFilter;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,16 +20,5 @@ public class ProyectoNavesApplication  extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoNavesApplication.class, args);
 	}
-
-	
-	  @Bean
-	    public FilterRegistrationBean filterRegistrationBean() {
-	        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-	        StrutsPrepareAndExecuteFilter struts = new StrutsPrepareAndExecuteFilter();
-	        registrationBean.setFilter(struts);
-	        registrationBean.setOrder(1);
-	        return registrationBean;
-	    }
-
 	
 }
